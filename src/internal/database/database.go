49a32574e9861df3958f6475830840624c5f710d
Matheus F Prado <matheusfenoliodoprado@outlook.com>
2022-09-10 13:50:03 -0300

    Removed hard coded connection string

--- src/internal/database/database.go
+++ src/internal/database/database.go
@@ -1,7 +1,9 @@
 package database
 
 import (
+	"fmt"
 	"go-api/src/internal/customer"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -14,17 +16,15 @@ var (
 func InitDatabase() {
 	var err error
 
-	// dsn := fmt.Sprintf(
-	// 	"host=%s user=%s password=%s dbname=%s port=%v sslmode=%s TimeZone=%s",
-	// 	os.Getenv("HOST"),
-	// 	os.Getenv("USERNAME"),
-	// 	os.Getenv("PASSWORD"),
-	// 	os.Getenv("DATABSE"),
-	// 	os.Getenv("DB_PORT"),
-	// 	os.Getenv("SDSLMODE"),
-	// 	os.Getenv("TIMEZONE"))
-
-	dsn := "host=192.168.1.16 user=postgres password=postgres dbname=postgres port=5432 sslmode=disable TimeZone=UTC"
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%v sslmode=%s TimeZone=%s",
+		os.Getenv("HOST"),
+		os.Getenv("USERNAME"),
+		os.Getenv("PASSWORD"),
+		os.Getenv("DATABSE"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("SDSLMODE"),
+		os.Getenv("TIMEZONE"))
 
 	DBConnection, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
